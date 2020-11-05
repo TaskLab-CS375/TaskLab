ca5ac7621dd2b1732faec04e125202662d18b100
Brian Chen <bc844@drexel.edu>
2020-11-05 13:54:44 -0500

    Handle login

--- tasklab/src/App.js
+++ tasklab/src/App.js
@@ -5,6 +5,7 @@ import {
     Route,
     Switch
 } from 'react-router-dom';
+import Login from './components/Login'
 
 function App() {
     return (
@@ -13,7 +14,7 @@ function App() {
                 <div className="container d-flex align-items-center flex-column">
                     <Switch>
                         <Route path="/login">
-
+                            <Login />
                         </Route>
                     </Switch>
                 </div>
--- tasklab/src/components/Login.js
+++ tasklab/src/components/Login.js
@@ -1,12 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class Login extends Component {
+export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: "",
-            password: "",
-            message: ""
+            password: ""
         }
+    }
+
+    handleInputChange = (event) => {
+        const {value, name} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                this.props.history.push('/');
+            } else {
+                throw new Error(res.error);
+            }
+        }).catch(err => {
+            console.log(err);
+            alert('Error logging in. Please try again.')
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Login Below!</h1>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
+                    required
+                />
+                <input type="submit" value="Submit"/>
+            </form>
+        );
     }
 }
