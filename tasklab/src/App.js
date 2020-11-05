5a3f0bb71df3e868bb4a1fed31ed53f3eb7908f2
Brian Chen <bc844@drexel.edu>
2020-11-05 12:14:45 -0500

    Setup initial route for login

--- tasklab/src/App.js
+++ tasklab/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <div className="container d-flex align-items-center flex-column">
+                    <Switch>
+                        <Route path="/login">
+
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
