d6c18f007faae91f1a689fb0166bdd656d2bec06
Brian Chen <bc844@drexel.edu>
2020-11-05 12:49:21 -0500

    Add register route to backend

--- app/server.js
+++ app/server.js
@@ -21,6 +21,41 @@ pool.connect().then(function () {
     console.log(`Connected to database ${env.database}`);
 });
 
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+app.post('/api/register', function (req, res) {
+    const { email, password } = req.body;
+
+    if(email === undefined || password === undefined) {
+        res.status(401).send();
+        return;
+    }
+
+    pool.query(
+        'SELECT password FROM users WHERE email = $1',
+        [email]
+    ).then(function (response) {
+        if (response.rows.length !== 0) {
+            return res.status(401).send('Email already exist!');
+        }
+    });
+
+    bcrypt
+        .hash(password, saltRounds)
+        .then(function (hashedPassword) {
+            pool.query(
+                'INSERT INTO users (email, password) VALUES ($1, $2)',
+                [email, hashedPassword]
+            ).then(function (response) {
+                res.status(200).send(response);
+            }).catch(function (error) {
+                res.status(500).send(error);
+            })
+        })
+})
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
