86054c1bf2aa1119b27ad2cdff854fa8b19dbe5b
Brian Chen <bc844@drexel.edu>
2020-11-12 14:21:44 -0500

    Remove register from navbar

--- tasklab/src/components/Nav.js
+++ tasklab/src/components/Nav.js
@@ -26,9 +26,6 @@ export default class Nav extends Component {
                             <li className="nav-item">
                                 <Link className="nav-link" to="/login">Login</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">Register</Link>
-                            </li>
                         </ul>
                     </div>
                 </nav>
